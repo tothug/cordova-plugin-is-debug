263005b7ddfd33f3fdde855fd7bcf9ae7e5448d8
yellow <totalhuang@gmail.com>
2018-10-19 23:18:48 +0800

    简化Debug标记获取方式

--- src/android/IsDebug.java
+++ src/android/IsDebug.java
@@ -1,24 +1,14 @@
 package me.mattlewis.isdebug;
 
-import android.content.Context;
-import android.content.pm.ApplicationInfo;
-import android.content.pm.PackageManager.NameNotFoundException;
-
-import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.BuildConfig;
 import org.apache.cordova.CallbackContext;
-
+import org.apache.cordova.CordovaPlugin;
 import org.json.JSONArray;
 import org.json.JSONException;
 
 public class IsDebug extends CordovaPlugin {
-
-    private static Context ctx;
-
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-
-        ctx = this.cordova.getActivity().getApplicationContext();
-
         if (action.equals("getIsDebug")) {
             callbackContext.success(this.isDebug() ? 1 : 0);
         } else {
@@ -28,16 +18,6 @@ public class IsDebug extends CordovaPlugin {
     }
 
     private boolean isDebug() {
-        try {
-            if ((ctx.getPackageManager().getPackageInfo(
-                ctx.getPackageName(), 0).applicationInfo.flags &
-                ApplicationInfo.FLAG_DEBUGGABLE) != 0) {
-                //Debug and development mode
-                return true;
-            }
-        } catch (NameNotFoundException e){
-            // do nothing
-        }
-        return false;
+        return BuildConfig.DEBUG;
     }
 }
